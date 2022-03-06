2fd334cb5871a38312c040ef4a811645117ef9bb
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-03-06 10:02:21 +0530

    Update afk.py

--- Flare_Robot/modules/afk.py
+++ Flare_Robot/modules/afk.py
@@ -173,10 +173,10 @@ __mod_name__ = "AFK"
  
 
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk)
-AFK_REGEX_HANDLER = MessageHandler(Filters.regex("(?i)brb"), afk)
-NO_AFK_HANDLER = MessageHandler(
-    Filters.all & Filters.chat_type.groups, no_longer_afk
+AFK_REGEX_HANDLER = DisableAbleMessageHandler(
+    Filters.regex(r"^(?i)brb(.*)$"), afk, friendly="afk"
 )
+NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.group, no_longer_afk)
 AFK_REPLY_HANDLER = MessageHandler(
     Filters.all & Filters.chat_type.groups, reply_afk
 )
