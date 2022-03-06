7787ef5ee648b9d338d45c09fe25f5ad1eead8b8
ultroi <86416271+ultroi@users.noreply.github.com>
2022-03-06 07:10:25 +0530

    Update tools.py

--- Flare_Robot/modules/tools.py
+++ Flare_Robot/modules/tools.py
@@ -21,5 +21,12 @@ ADMINS ONLY
 *Webss*
 `/webss` [URL] - Take A Screenshot Of A Webpage 
 
-
+*Sed/Regex*
+ • `s/<text1>/<text2>(/<flag>)`*:* Reply to a message with this to perform a sed operation on that message, replacing all \
+occurrences of 'text1' with 'text2'. Flags are optional, and currently include 'i' for ignore case, 'g' for global, \
+or nothing. Delimiters include `/`, `_`, `|`, and `:`. Text grouping is supported. The resulting message cannot be \
+larger than {}.
+*Reminder:* Sed uses some special characters to make matching easier, such as these: `+*.?\\`
+If you want to use these characters, make sure you escape them!
+*Example:* \\?.
 """
