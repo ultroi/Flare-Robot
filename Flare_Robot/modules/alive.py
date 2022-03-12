f261441cff6424a66a7d58b5ed586d3f9bdf76f5
ultroi <86416271+ultroi@users.noreply.github.com>
2022-03-13 01:19:49 +0530

    Update alive.py

--- Flare_Robot/modules/alive.py
+++ Flare_Robot/modules/alive.py
@@ -9,21 +9,21 @@ from Flare_Robot.events import register
 from Flare_Robot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/6266d4d7ce030b8a7cf2d.jpg"
+PHOTO = "https://telegra.ph/file/438ea17e93f284ae9fe9d.jpg"
 
 
 @register(pattern=("/alive"))
 async def awake(event):
-    TEXT = "**♡ I,m ғʟᴀʀᴇ ʀᴏʙᴏᴛ 愛** \n\n"
+    TEXT = "**♡ I,m Pikachu 愛** \n\n"
     TEXT += f"**♡ I'm Working With sᴇxʏ Speed** \n\n"
-    TEXT += f"**♡ ғʟᴀʀᴇ: LATEST Version** \n\n"
-    TEXT += f"**♡ My Creator: [ ᴀsᴛᴀ](http://t.me/Asta_silva02)** \n\n"
-    TEXT += f"**♡ ᴀɴʏ ɪssᴜᴇs ᴄᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ @Freia_Support** \n\n"
-    TEXT += "**♡ ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ 💘💘💘**"
+    TEXT += f"**♡ Pikachu: LATEST Version** \n\n"
+    TEXT += f"**♡ My Creator: [RYUK](http://t.me/Weeb_lover)** \n\n"
+    TEXT += f"**♡ ᴀɴʏ ɪssᴜᴇs ᴄᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ @PikachuHelpSupport** \n\n"
+    TEXT += "**♡ ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ 💗**"
     BUTTON = [
         [
-            Button.url("📢 Updates", "https://t.me/Freia_Updates"),
-            Button.url("🚑 Support", "https://t.me/Freia_Support"),
+            Button.url("📢 Updates", "https://t.me/PikachuUpdates"),
+            Button.url("🚑 Support", "https://t.me/PikachuHelpSupport"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
